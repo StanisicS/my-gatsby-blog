940d38e9ccc0e1e4a329b884c09bd8cbac8a892d
Stevan Stanisic <stevan.stanisic@outlook.com>
2020-02-04 04:01:46 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,13 @@ module.exports = {
     author: `Stevan Stanisic`,
   },
   plugins: [
+{
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {

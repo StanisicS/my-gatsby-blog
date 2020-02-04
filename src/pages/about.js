0e1edc76a2355ff9ef1cdb27826b779f6fe37df2
Stevan Stanisic <stevan.stanisic@outlook.com>
2020-02-04 05:52:04 +0100

    img ql

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,32 +6,28 @@ import Layout from "../components/layout"
 
 export default ({ data }) => (
   <Layout>
-    <h1>About {data.site.siteMetadata.title}</h1>
+    <h1>About me</h1>
     <p>
       We're the only site running on your computer dedicated to showing the best
       photos and videos of pandas eating lots of food.
     </p>
-    <Img fluid={data.file.childImageSharp.fluid} />
+    <Img fixed={data.file.childImageSharp.fixed} />
   </Layout>
 )
 
 export const query = graphql`
   query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
+    # site {
+    #   siteMetadata {
+    #     title
+    #   }
+    # }
     file(relativePath: { eq: "src/images/img1.jpeg" }) {
       childImageSharp {
         # Specify the image processing specifications right in the query.
         # Makes it trivial to update as your page's design changes.
-        fluid {
-          aspectRatio
-          base64
-          sizes
-          src
-          srcSet
+        fixed(width: 125) {
+          ...GatsbyImageSharpFixed
         }
       }
     }

96a2373ba701d2e2d0761a4b4172fa43bb3f2a2f
Stevan Stanisic <stevan.stanisic@outlook.com>
2020-02-04 18:40:27 +0100

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,3 +41,12 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 }
+
+exports.onCreateWebpackConfig = ({ actions, getConfig }) => {
+  // Hack due to Tailwind ^1.1.0 using `reduce-css-calc` which assumes node
+  // https://github.com/bradlc/babel-plugin-tailwind-components/issues/39#issuecomment-526892633
+  const config = getConfig()
+  config.node = {
+    fs: "empty",
+  }
+}

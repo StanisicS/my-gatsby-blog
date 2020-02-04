97a3741f29b62777fc6863758f9107918965036e
Stevan Stanisic <stevan.stanisic@outlook.com>
2020-02-04 19:52:14 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,19 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
-import Header from "../components/header"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 export default () => (
   <Layout>
     <SEO title="Contact" />
-    <div style={{ color: `teal` }}>
-      <Link to="/">Home</Link>
-      <Header headerText="Contact" />
-      <h1>I'd love to talk! Email me at the address below</h1>
-      <p>
-        <a href="mailto:me@example.com">me@example.com</a>
-      </p>
-    </div>
+    <h1>I'd love to talk! Email me at the address below</h1>
+    <p>
+      <a href="mailto:me@example.com">me@example.com</a>
+    </p>
+    <Img fluid={data.file.childImageSharp.fluid} />
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "IMG-ae16660396470d4fb4903768a1d5fa9b-V.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 700) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`

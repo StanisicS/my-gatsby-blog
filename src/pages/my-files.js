5c2ce9b4d32ea77e1c93c5a51fe60e1ee2b220c4
Stevan Stanisic <stevan.stanisic@outlook.com>
2020-02-04 20:14:21 +0100

    Update my-files.js

--- src/pages/my-files.js
+++ src/pages/my-files.js
@@ -1,6 +1,8 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
+
 
 export default ({ data }) => {
   console.log(data)
